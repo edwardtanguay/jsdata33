9e33444b1e72a82a13ad5f5278a2e7ca2bda6f5a
Edward Tanguay (wk.win) <edward.tanguay@gefegmbh.local>
2020-07-03 18:00:34 +0200

    more eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,16 @@ module.exports = {
 		es6: true
 	},
 	rules: {
-		'strict': ['error', 'global'],
+		strict: ['error', 'global'],
 		'no-var': 'error',
-		'prefer-const' : 'error'
+		'prefer-const': 'error',
+		'one-var': ['error', 'never'],
+		camelcase: 'error',
+		'no-unused-vars': 'error',
+		'no-multi-assign': 'error',
+		quotes: ['error', 'single'],
+		'no-array-constructor': 'error',
+		'no-new-object': 'error',
+		'quote-props': ['error', 'as-needed']
 	},
 };
--- strict.js
+++ strict.js
@@ -5,4 +5,36 @@ console.log(city);
 const name = 'Win10';
 console.log(name);
 
-const name2 = 'kdjf';
+const server1 = 'kdjfkdjf';
+const server2 = 'iejfeife';
+
+console.log(server1);
+console.log(server2);
+
+
+const firstTotal = 5;
+let subtotal = 0;
+let total = 0;
+
+subtotal = firstTotal;
+total = subtotal;
+
+console.log('total: ' + total);
+console.log('subtotal: ' + subtotal);
+
+const serverName = 'Windows 2016 Server';
+console.log('server: ' + serverName);
+
+const days = ['Sunday', 'Monday', 'Wednesday'];
+console.log('days: ' + days.join(', '));
+
+let person = {};
+
+person = {
+	firstName: 'Harry',
+	lastName: 'Hopkins',
+	'first-role': 'unknown',
+	'second-role': 'missing'
+};
+console.log('person: ', person);
+
